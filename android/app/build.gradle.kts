7e69c0b61ff1cf38d3890df826e27c1d90c11269
apooravkaul <140042022+apooravkaul@users.noreply.github.com>
2026-07-02 15:54:32 +0530

    applicationId name change

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -17,7 +17,7 @@ if (hasKeystoreProperties) {
 }
 
 android {
-    namespace = "com.example.correctv1"
+    namespace = "com.alignpod.app"
     compileSdk = 36
     ndkVersion = flutter.ndkVersion
 
@@ -28,8 +28,7 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.correctv1"
+        applicationId = "com.alignpod.app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
