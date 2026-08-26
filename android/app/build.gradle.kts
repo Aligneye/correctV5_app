7f96436e452b70f94336e792f22f8a64c624ddfa
apooravkaul <140042022+apooravkaul@users.noreply.github.com>
2026-08-26 18:28:02 +0530

    connectivity

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -19,7 +19,7 @@ if (hasKeystoreProperties) {
 android {
     namespace = "com.alignpod.app"
     compileSdk = 36
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
